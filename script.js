bd5c9cab2849de563bbf5b2c1d34d94e391a57af
Yurika Namba <yurikanamba@gmail.com>
2020-04-03 22:01:07 +0900

    added all functionality and design

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const getMealButton = document.querySelector("#get_meal");
-const mealContainer = document.querySelector("#meal");
+const mealContainer = document.querySelector("#meal-section");
 
 getMealButton.addEventListener("click", getRecipe);
 
@@ -18,23 +18,34 @@ function getRecipe() {
 function generateRecipe(newRecipe) {
   //title
   const recipeTitleEl = document.createElement("h2");
+  recipeTitleEl.id = "recipe_title";
   recipeTitleEl.innerHTML = newRecipe.strMeal;
   mealContainer.appendChild(recipeTitleEl);
 
   //tags
+  const recipeTagsEl = document.createElement("div");
   const recipeCountryEl = document.createElement("p");
   const recipeCategoryEl = document.createElement("p");
+  recipeTagsEl.classList.add("tag-container");
+  recipeCountryEl.classList.add("tag");
+  recipeCategoryEl.classList.add("tag");
   recipeCountryEl.innerHTML = newRecipe.strArea;
   recipeCategoryEl.innerHTML = newRecipe.strCategory;
-  mealContainer.appendChild(recipeCountryEl);
-  mealContainer.appendChild(recipeCategoryEl);
+  recipeTagsEl.appendChild(recipeCountryEl);
+  recipeTagsEl.appendChild(recipeCategoryEl);
+  mealContainer.appendChild(recipeTagsEl);
+
+  //image and ingredients container
+  const recipeImgIngContEl = document.createElement("div");
+  recipeImgIngContEl.classList.add("ImgIngCont");
 
   //image
   const recipeImageEl = document.createElement("IMG");
   recipeImageEl.src = newRecipe.strMealThumb;
-  mealContainer.appendChild(recipeImageEl);
 
   //Ingredients and Measure
+  const ingredientsCont = document.createElement("div");
+  ingredientsCont.classList.add("recipe-ingredients");
   const ingredientsHeader = document.createElement("h3");
   ingredientsHeader.innerText = "Ingredients";
   const recipeIngMeasEl = document.createElement("ul");
@@ -46,22 +57,28 @@ function generateRecipe(newRecipe) {
       recipeIngMeasEl.appendChild(recipeItem);
     }
   }
-  mealContainer.appendChild(ingredientsHeader);
-  mealContainer.appendChild(recipeIngMeasEl);
+  ingredientsCont.appendChild(ingredientsHeader);
+  ingredientsCont.appendChild(recipeIngMeasEl);
+  recipeImgIngContEl.appendChild(recipeImageEl);
+  recipeImgIngContEl.appendChild(ingredientsCont);
+  mealContainer.appendChild(recipeImgIngContEl);
 
   //video
-  const recipeVideoEl = document.createElement("object");
-  recipeVideoEl.data = newRecipe.strYoutube.replace("watch?", "embed/");
+  const recipeVideoEl = document.createElement("iframe");
+  recipeVideoEl.id = "recipe_video";
+  recipeVideoEl.src = `https://youtube.com/embed/${newRecipe.strYoutube.slice(
+    -11
+  )}`;
   recipeVideoEl.frameborder = "0";
   recipeVideoEl.allow =
     "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
-  console.log(newRecipe.strYoutube.replace("watch?v=", "embed/"));
   mealContainer.appendChild(recipeVideoEl);
 
   //instructions
   const instructionsHeader = document.createElement("h3");
   instructionsHeader.innerText = "Instructions";
   const recipeInstructionEl = document.createElement("p");
+  recipeInstructionEl.id = "recipe_instructions";
   recipeInstructionEl.innerText = newRecipe.strInstructions;
   mealContainer.appendChild(instructionsHeader);
   mealContainer.appendChild(recipeInstructionEl);
